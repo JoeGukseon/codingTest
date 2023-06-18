a639bf8c0f84f5c75c6b52766c419a1834ee85cd
JoeGukseon <zipcks1381@gmail.com>
2023-06-19 01:39:57 +0900

    lv2 피로도 수정

--- src/lv2/피로도.java
+++ src/lv2/피로도.java
@@ -7,26 +7,25 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class 피로도 {
-    int answer;
-
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons, visited);
-        return answer;
+
+        return dfs(0, k, dungeons, visited);
     }
 
-    private void dfs(int depth, int k, int[][] dungeons, boolean[] visited) {
-
+    private int dfs(int depth, int k, int[][] dungeons, boolean[] visited) {
+        int answer=0;
         for (int i = 0; i < dungeons.length; i++) {
             System.out.println("depth = " + depth + ", k = " + k + ", dungeons = " + Arrays.deepToString(dungeons) + ", visited = " + Arrays.toString(visited)+", i = "+i);
             if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                dfs(depth + 1, k - dungeons[i][1], dungeons, visited);
+                answer = Math.max(answer, dfs(depth + 1, k - dungeons[i][1], dungeons, visited));
                 visited[i] = false;
             }
 
         }
-        answer = Math.max(answer, depth);
+        System.out.println("answer = " + answer);
+        return Math.max(answer, depth);
     }
 
     @Test
