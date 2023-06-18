d1bbe1c02595fdd33f8838a9968130558ea20449
JoeGukseon <zipcks1381@gmail.com>
2023-06-19 01:33:20 +0900

    lv2 타겟넘버 테스트 수정

--- src/lv2/타겟넘버.java
+++ src/lv2/타겟넘버.java
@@ -7,26 +7,26 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class 타겟넘버 {
-    int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(0, numbers, target, 0);
-        return answer;
+
+        return dfs(0, numbers, target, 0);
     }
 
-    private void dfs(int depth, int[] numbers, int target, int result) {
+    private int dfs(int depth, int[] numbers, int target, int result) {
+        int answer = 0;
         if (depth == numbers.length) {
             if(result == target){
                 System.out.println("depth = " + depth + ", numbers = " + Arrays.toString(numbers) + ", target = " + target + ", result = " + result);
                 answer++;}
         } else {
-            dfs(depth + 1, numbers, target, result + numbers[depth]);
-            dfs(depth + 1, numbers, target, result - numbers[depth]);
+            answer += dfs(depth + 1, numbers, target, result + numbers[depth]);
+            answer += dfs(depth + 1, numbers, target, result - numbers[depth]);
         }
+        return answer;
     }
     @Test
     public void test() {
         assertEquals(5, solution(new int[]{1, 1, 1, 1, 1}, 3));
-        answer = 0;
         assertEquals(2, solution(new int[]{4, 1, 2, 1}, 4));
     }
 
